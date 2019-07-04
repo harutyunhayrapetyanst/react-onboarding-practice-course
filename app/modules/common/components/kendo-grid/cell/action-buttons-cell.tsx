3cde983602f1382bfa1ce5c49821707fd1844dc1
harutyunhayrapetyanst <hhayrapetyan@servicetitan.com>
2019-07-04 14:45:14 +0400

    #5: Create a kendo-grid cell for action buttons

---
+++ app/modules/common/components/kendo-grid/cell/action-buttons-cell.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { GridCell } from '@progress/kendo-react-grid';
+import { Button } from '@servicetitan/design-system';
+
+export class ActionButtonsCell extends GridCell {
+
+    onEditClick = () => {
+        console.log('edit');
+    };
+
+    onSaveClick = () => {
+        console.log('save');
+    };
+
+
+    onDeleteClick = () => {
+        console.log('delete');
+    };
+
+    onCancelClick = () => {
+        console.log('cancel');
+    };
+
+    render() {
+        const { dataItem: { inEdit } } = this.props;
+        if (inEdit) {
+            return (
+                <td>
+                    <Button text onClick={this.onCancelClick}>Cancel</Button>
+                    <Button text primary onClick={this.onSaveClick}>Save</Button>
+                </td>
+            );
+        } else {
+            return (
+                <td>
+                    <Button text primary onClick={this.onEditClick}>Edit</Button>
+                    <Button text negative onClick={this.onDeleteClick}>Delete</Button>
+                </td>
+            );
+        }
+    }
+}
